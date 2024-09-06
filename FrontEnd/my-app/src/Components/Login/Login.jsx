d1bd8a4eaaf0fa135ee3ef6a51b5a6a6ccde87d5
RAYAN MOKDAD <2232174@cmontmorency.qc.ca>
2024-09-06 16:43:44 -0400

    fait: login & signup

--- FrontEnd/my-app/src/Components/Login/Login.jsx
+++ FrontEnd/my-app/src/Components/Login/Login.jsx
@@ -1,31 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 
 const Login = () => {
+  const [action, setAction] = useState("Login");
+
   return (
     <div className="container">
       <div className="header">
-        <div className="text">Sign up</div>
+        <div className="text">{action}</div>
         <div className="underline"></div>
       </div>
       <div className="inputs">
-        <div className="input">
-          <div className="text">Username</div>
-          <input type="text" />
-          <div className="input">
-            <div className="text">Email</div>
-            <input type="email" />
-            <div className="text">Password</div>
-            <input type="password" />
+        {action === "Sign Up" && (
+          <div className="signup-inputs">
+            <div className="input">
+              <div className="text">Username</div>
+              <input type="text" placeholder="Username" />
+            </div>
+            <div className="input">
+              <div className="text">Email</div>
+              <input type="email" placeholder="Email Id" />
+            </div>
+            <div className="input">
+              <div className="text">Password</div>
+              <input type="password" placeholder="Password" />
+            </div>
           </div>
-        </div>
+        )}
+        {action === "Login" && (
+          <div className="login-inputs">
+            <div className="input">
+              <div className="text">Username</div>
+              <input type="text" placeholder="Username" />
+            </div>
+            <div className="input">
+              <div className="text">Password</div>
+              <input type="password" placeholder="Password" />
+            </div>
+          </div>
+        )}
       </div>
       <div className="forgot-password">
         Lost Password? <span>Click here!</span>
       </div>
       <div className="submit-container">
-        <div className="submit">Sign up</div>
-        <div className="submit">Login</div>
+        <div
+          className={`submit ${action === "Sign Up" ? "active" : ""}`}
+          onClick={() => setAction("Sign Up")}
+        >
+          Sign up
+        </div>
+        <div
+          className={`submit ${action === "Login" ? "active" : ""}`}
+          onClick={() => setAction("Login")}
+        >
+          Login
+        </div>
       </div>
     </div>
   );
